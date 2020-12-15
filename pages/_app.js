f475378b56a0135c9dcf79fc505139d2b7bc57ad
Bao Nguyen <bao.nguyen201275@gmail.com>
2020-12-15 10:23:21 +0700

    setup chakra-ui and amplify

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ThemeProvider, theme, CSSReset } from "@chakra-ui/react";
+import Amplify from "aws-amplify";
+
+import awsconfig from "../src/aws-exports";
+import "../styles/globals.css";
+
+Amplify.configure(awsconfig);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
